e30a9f4ee0b9b925cd22c606bc26b65381df2b25
nulledge <the.nulledge@gmail.com>
2021-05-24 21:28:24 +0900

    스프링 부트 초기 코드를 추가함

---
+++ src/main/java/none/study_spring/TestController.java
@@ -0,0 +1,13 @@
+package none.study_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/")
+	String getHello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/none/study_spring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package none.study_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
