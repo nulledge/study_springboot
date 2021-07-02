496630eafaa79ab02358bb0b8f9d2e789bcb50f1
nulledge <the.nulledge@gmail.com>
2021-07-02 17:40:40 +0900

    Spring security 빌드 변경 및 Member 등록 시 암호에 Argon2 적용

---
+++ src/main/java/nulledge/study_spring/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package nulledge.study_spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().permitAll()
+				.and()
+			.csrf()
+				.disable()
+		;
+	}
+	
+}
